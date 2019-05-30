2b9864fce0ebee4eae792c3bc4e23d05fe86a05a
Amanda Maharjan <maharjanamanda@gmail.com>
2019-05-30 23:02:12 +0545

    new routes to get a detail and an all page

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -32,6 +32,65 @@ router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => 
     .catch(err => res.status(404).json(err));
 })
 
+// @route GET api/profile/handle/:handle
+// @desc Get profile by handle
+// @access public
+router.get('/handle/:handle', (req, res) => {
+  const errors = {};
+
+  Profile.findOne({ handle: req.params.handle })
+    .populate('user', ['name', 'avatar'])
+    .then(profile => {
+      if(!profile) {
+        errors.noprofile = 'There is no profile for this user';
+        res.status(404).json(errors)
+      }
+
+      res.json(profile);
+    })
+    .catch(err => res.status(404).json({ profile: 'There is no profile for this user' }));
+});
+
+// @route GET api/profile/user/:user_id
+// @desc Get profile by user_id
+// @access public
+router.get('/user/:user_id', (req, res) => {
+  const errors = {};
+
+  Profile.findOne({ handle: req.params.user_id })
+    .populate('user', ['name', 'avatar'])
+    .then(profile => {
+      if(!profile) {
+        errors.noprofile = 'There is no profile for this user';
+        res.status(404).json(errors)
+      }
+
+      res.json(profile);
+    })
+    .catch(err => res.status(404).json({ profile: 'There is no profile for this user' }));
+});
+
+// @route GET api/profile/all
+// @desc Get all profiles
+// @access public
+router.get('/all', (req, res) => {
+  const errors = {};
+
+  Profile.find()
+    .populate('user', ['name', 'avatar'])
+    .then(profiles => {
+      if(!profiles){
+        errors.noprofile = 'There is no profile for this user';
+        return res.status(404).json(errors);
+      }
+      res.json(profiles);
+    })
+    .catch(err => {
+      res.status(400).json({ profile: 'There are no profiles' })
+    })
+});
+
+
 // @route POST api/profile
 // @desc Create or update profile user profile
 // @access private
