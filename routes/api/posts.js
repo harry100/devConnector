4a711f498590cf07008727e220adf0cf77160717
Amanda Maharjan <maharjanamanda@gmail.com>
2019-06-01 13:45:06 +0545

    Post lists, detail and delete page working

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -6,6 +6,9 @@ const passport = require('passport');
 // Post Model
 const Post = require('../../models/Post');
 
+// Progile Model
+const Progile = require('../../models/Profile');
+
 // Validation
 const validatePostInput = require('../../validation/post');
 
@@ -13,6 +16,27 @@ const validatePostInput = require('../../validation/post');
 // @desc    Tests post route
 // @access  Public
 router.get('/test', (req, res) => res.json({msg: "Posts Works"}));
+
+// @route   GET api/posts/
+// @desc    Get Posts
+// @access  Public
+router.get('/', (req, res) => {
+
+  Post.find()
+    .sort({ date: -1})
+    .then(posts => res.json(posts))
+    .catch(err => res.status(400).json(err))
+})
+
+// @route   GET api/posts/:id
+// @desc    Get Post by id
+// @access  Public
+router.get('/:id', (req, res) => {
+
+  Post.findById(req.params.id)
+    .then(post => res.json(post))
+    .catch(err => res.status(400).json({err: 'No post found found'}))
+})
 
 // @route   POST api/posts/
 // @desc    Create Posts
@@ -33,6 +57,26 @@ router.post('/', passport.authenticate('jwt', {session: false}), (req, res) => {
   });
 
   newPost.save().then(post => res.json(post));
+})
+
+// @route   DELETE api/posts/:id
+// @desc    Delete Posts
+// @access  Private
+router.delete('/:id', passport.authenticate('jwt', {session: false}), (req, res) => {
+  Profile.findOne({ user: req.user.id })
+    .then(profile => {
+      Post.findById(req.params.id)
+        .then(post => {
+          //Check for post owner
+          if(post.user.toString() !== req.user.id){
+            return res.status(401).json({ notAuthorized: 'User is not authorized' });
+          }
+
+          //Delete
+          post.remove().then(() => res.json({succed: true}));
+        })
+    })
+    .catch(err => res.status(404).json({ postNotFound: 'No post found' }))
 })
 
 module.exports = router;
