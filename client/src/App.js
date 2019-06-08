3c89f04794ba61ceb1720b44021e4008aeb6c832
Amanda Maharjan <maharjanamanda@gmail.com>
2019-06-08 19:49:27 +0545

    store, reducer and devtool setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
+import store from './store';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
@@ -8,19 +10,22 @@ import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import './App.css';
 
+
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Navbar />
-        <Route path='/' exact component={Landing} />
-        <div className="container">
-          <Route path='/register' exact component={Register} />
-          <Route path='/login' exact component={Login} />
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route path='/' exact component={Landing} />
+          <div className="container">
+            <Route path='/register' exact component={Register} />
+            <Route path='/login' exact component={Login} />
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
-    </Router>
+      </Router>
+    </Provider>
   );
 }
 
