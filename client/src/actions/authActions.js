575bf7b39bfc883ad7efcce969f693517b5d6913
Amanda Maharjan <maharjanamanda@gmail.com>
2019-07-24 22:04:38 +0545

    login user action reduccer setup

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,12 +1,14 @@
 import axios from 'axios';
-import { GET_ERRORS } from './types';
+import setAuthToken from '../utils/setAuthToken';
+import jwt_decode from 'jwt-decode';
+
+import { GET_ERRORS, SET_CURENT_USER } from './types';
 
 //Register user
 export const registerUser = (userData, history) => dispatch => {
   axios
     .post('http://localhost:4000/api/users/register', userData)
-    // .then(res => history.push('/login'))
-    .then(res => console.log('/login'))
+    .then(res => history.push('/login'))
     .catch(err => {
       console.log(err)
       dispatch({
@@ -16,3 +18,35 @@ export const registerUser = (userData, history) => dispatch => {
     }
     );
 };
+
+//Login Get user token
+export const loginUser = (userData) => dispatch => {
+  axios
+    .post('http://localhost:4000/api/users/login', userData)
+    .then(res => {
+      //Save to localstorage
+      const { token } = res.data;
+      //Set token to localstorage
+      localStorage.setItem('jwtToken', token);
+      //Set token to Auth Header
+      setAuthToken(token);
+
+      //Decode token to get user data
+      const decoded = jwt_decode(token);
+      //Set current user
+      dispatch(setCurrentUser(decoded));
+    })
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      }))
+}
+
+//Set logged in user
+export const setCurrentUser = (decoded) => {
+  return{
+    type: SET_CURENT_USER,
+    payload: decoded
+  }
+}
