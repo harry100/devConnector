e45d7b29fd2cc741215bf5d1d02f30ec8725e5ce
Amanda Maharjan <maharjanamanda@gmail.com>
2019-09-03 13:59:48 +0545

    create component for adding experience

--- client/src/App.js
+++ client/src/App.js
@@ -18,6 +18,7 @@ import Login from './components/auth/Login';
 import Dashboard from './components/dashboard/Dashboard';
 import CreateProfile from './components/create-profile/CreateProfile';
 import EditProfile from './components/edit-profile/EditProfile';
+import AddExperience from './components/add-credentials/AddExperience';
 
 import './App.css';
 
@@ -61,6 +62,9 @@ function App() {
             </Switch>
             <Switch>
               <PrivateRoute path='/edit-profile' exact component={EditProfile} />
+            </Switch>
+            <Switch>
+              <PrivateRoute path='/add-experience' exact component={AddExperience} />
             </Switch>
           </div>
           <Footer />
