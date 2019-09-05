49328c2042fd638ddf441d858b08855c8bd19bed
Amanda Maharjan <maharjanamanda@gmail.com>
2019-09-05 10:27:11 +0545

    profile created

--- client/src/components/profile/ProfileHeader.js
+++ client/src/components/profile/ProfileHeader.js
@@ -1,10 +1,97 @@
 import React, { Component } from 'react';
 
+import isEmpty from '../../validation/is-empty';
+
 class ProfileHeader extends Component {
   render() {
+    const { profile } = this.props;
+
     return (
-      <div>
-        <h1> TODO: Profile Header </h1>
+      <div className="row">
+        <div className="col-md-12">
+          <div className="card card-body bg-info text-white mb-3">
+            <div className="row">
+              <div className="col-4 col-md-3 m-auto">
+                <img
+                  className="rounded-circle"
+                  src={profile.user.avatar}
+                  alt=""
+                />
+              </div>
+            </div>
+            <div className="text-center">
+              <h1 className="display-4 text-center">{profile.user.name}</h1>
+              <p className="lead text-center">
+                {profile.status}{' '}
+                {isEmpty(profile.company) ? null : (
+                  <span>at {profile.company}</span>
+                )}
+              </p>
+              {isEmpty(profile.location) ? null : <p>{profile.location}</p>}
+              <p>
+                {isEmpty(profile.website) ? null : (
+                  <a
+                    className="text-white p-2"
+                    href={profile.website}
+                    target="_blank"
+                  >
+                    <img src="/images/domain.svg" style={{height: "37px", width: "37px"}} alt="domain"/>
+                  </a>
+                )}
+
+                {isEmpty(profile.social && profile.social.twitter) ? null : (
+                  <a
+                    className="text-white p-2"
+                    href={profile.social.twitter}
+                    target="_blank"
+                  >
+                    <img src="/images/twitter-logo-button.svg" style={{height: "37px", width: "37px"}} alt="twitter"/>
+                  </a>
+                )}
+
+                {isEmpty(profile.social && profile.social.facebook) ? null : (
+                  <a
+                    className="text-white p-2"
+                    href={profile.social.facebook}
+                    target="_blank"
+                  >
+                    <img src="/images/facebook-logo-button.svg" style={{height: "37px", width: "37px"}} alt="facebook"/>
+                  </a>
+                )}
+
+                {isEmpty(profile.social && profile.social.linkedin) ? null : (
+                  <a
+                    className="text-white p-2"
+                    href={profile.social.linkedin}
+                    target="_blank"
+                  >
+                    <img src="/images/linkedin-logo-button.svg" style={{height: "37px", width: "37px"}} alt="linkedin"/>
+                  </a>
+                )}
+
+                {isEmpty(profile.social && profile.social.youtube) ? null : (
+                  <a
+                    className="text-white p-2"
+                    href={profile.social.youtube}
+                    target="_blank"
+                  >
+                    <img src="/images/youtube-logo-button.svg" style={{height: "37px", width: "37px"}} alt="youtube"/>
+                  </a>
+                )}
+
+                {isEmpty(profile.social && profile.social.instagram) ? null : (
+                  <a
+                    className="text-white p-2"
+                    href={profile.social.instagram}
+                    target="_blank"
+                  >
+                    <img src="/images/instagram-logo-button.svg" style={{height: "37px", width: "37px"}} alt="instagram"/>
+                  </a>
+                )}
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
