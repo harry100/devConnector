7609db8e18de7ade06bba6cde999df1bc095fa89
Amanda Maharjan <maharjanamanda@gmail.com>
2019-09-09 14:24:52 +0545

    pushed route to not found if an unknown handle is usedd

--- client/src/App.js
+++ client/src/App.js
@@ -22,6 +22,7 @@ import AddExperience from './components/add-credentials/AddExperience';
 import AddEducation from './components/add-credentials/AddEducation';
 import Profiles from './components/profiles/Profiles';
 import Profile from   './components/profile/Profile';
+import NotFound from './components/not-found/NotFound';
 
 import './App.css';
 
@@ -74,6 +75,7 @@ function App() {
             <Switch>
               <PrivateRoute path='/add-education' exact component={AddEducation} />
             </Switch>
+            <Route path='/not-found' exact component={NotFound} />
           </div>
           <Footer />
         </div>
