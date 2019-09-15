86abcbd25d71e20824a1fdcca9436677a230d81e
Amanda Maharjan <maharjanamanda@gmail.com>
2019-09-15 09:38:21 +0545

    prepared for deploy

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
-var cors = require('cors')
+var cors = require('cors');
+const path = require('path');
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
@@ -40,6 +41,15 @@ require('./config/passport.js')(passport);
 app.use('/api/users', users);
 app.use('/api/posts', posts);
 app.use('/api/profile', profile);
+
+//Serve static assets if in production
+if(process.env.NODE_ENV === 'production'){
+  //set a static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
 
 const port = process.env.PORT || 4000;
 
